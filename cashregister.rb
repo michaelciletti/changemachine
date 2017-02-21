5e1350c1b0afb3aabaad65946e8498051ec35566
michaelciletti <michaelciletti@gmail.com>
2017-02-21 09:08:22 -0500

    altered function added hash = to 2nd parameter

--- cashregister.rb
+++ cashregister.rb
@@ -1,6 +1,16 @@
 # In: Get money in cents
 # Out: Change the minimum quantity of coins
-def makechange(amount, values = VALUES)
+# def makechange(amount, values = VALUES)
+#   change = {}  
+#   values.each{|coin, value|
+#     num, amount = amount.divmod(value)
+#     change[coin] = num if num > 0}
+#   return change
+# end
+ 
+# VALUES = {:Quarters => 25,:Dimes => 10,:Nickels =>  5,:Pennies =>  1,}
+
+def makechange(amount, values = {:Quarters => 25, :Dimes => 10, :Nickels =>  5, :Pennies =>  1,})
   change = {}  
   values.each{|coin, value|
     num, amount = amount.divmod(value)
@@ -8,5 +18,4 @@ def makechange(amount, values = VALUES)
   return change
 end
  
-VALUES = {:Quarters => 25,:Dimes => 10,:Nickels =>  5,:Pennies =>  1,}
-
+ 
